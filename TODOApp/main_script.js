39cd4140605180983b58a1e406e986dd5e46dcf2
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2023-12-02 18:00:43 +0100

    Add disclaimer toggle function
    had to redo the function that resets the list and made the disclaimer toggle if the list is empty or not but the hide class won't be removed

--- TODOApp/main_script.js
+++ TODOApp/main_script.js
@@ -5,17 +5,25 @@ const submitBtn = document.querySelector(".submit");
 const resetBtn = document.querySelector(".reset");
 const taskList = document.querySelector(".task-list");
 const inputField = document.querySelector("input");
+const disclaimerMessage = document.querySelector(".disclaimer");
 /* The idea behind defining a variable with that holds all the added tasks is to be able to load them instantly from the LocalStorage when the pages reloads instead of listening fro the event*/
 let tasks = JSON.parse(localStorage.getItem("data")) || [];
+
 
 function saveData() {
     // when using the localStorage object , transform your data into JSON to easily save it
     localStorage.setItem("data", JSON.stringify(tasks));
 }
 
+function removeListElements() {
+    const listElements = taskList.querySelectorAll("li");
+    listElements.forEach((currentItem)=> taskList.removeChild(currentItem));
+}
+
 function resetData() {
     tasks = [];
-    taskList.innerHTML = "";
+    removeListElements();
+    disclaimerMessage.classList.remove("hide");
     localStorage.clear();
 }
 
@@ -34,6 +42,7 @@ function getTasks(e) {
 }
 
 function populateList(userTask) {
+    disclaimerMessage.classList.add("hide");
     const index = tasks.indexOf(userTask);
 
     taskList.innerHTML = taskList.innerHTML + `
@@ -53,6 +62,24 @@ function setTaskStatus(e) {
        tasks[identifier].done = currentCheckbox.checked;
     }
 }
+
+// Watching Over the list inner changes to toggle the disclaimer
+
+/*const callback = function(changesRecords) {
+    console.log(changesRecords);
+    const currentRecord = changesRecords[0];
+    console.log(taskList.childElementCount);
+    disclaimerMessage.classList.toggle("hide", taskList.childElementCount === 1);
+    //taskList.childElementCount === 1 ? disclaimerMessage.classList.remove("hide"): disclaimerMessage.classList.add("hide");
+}
+const listObserver = new MutationObserver(callback);
+const configurationObj = {
+    childList: true,
+    subtree: true,
+}
+
+
+listObserver.observe(taskList, configurationObj);*/
 
 addTaskBtn.addEventListener("click", getTasks);
 submitBtn.addEventListener("click", saveData);
