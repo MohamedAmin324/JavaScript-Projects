b05a6699d97dcc93f0d10d1c3fceda5e14e9b28a
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2023-12-18 20:09:36 +0100

    Add display feature

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const calculatorBtnSection = document.querySelector(".calculator__btn-section");
+const currentOperationDisplay = document.querySelector(".calculator__current-operation");
+const resultDisplay = document.querySelector(".calculator__result");
+
+function populateDisplay(e) {
+    const clickedBtn = e.target;
+
+    if(clickedBtn.classList.contains("operation-key")) return;
+    if(!clickedBtn.matches("span")) return;
+
+    if(["x", "+", "/", "-"].includes(clickedBtn.innerText)) {
+        resultDisplay.innerText = `${currentOperationDisplay.innerText} ${clickedBtn.innerText}`;
+        currentOperationDisplay.innerText = "0";
+        return;
+    }
+
+    if(currentOperationDisplay.innerText === "0") {
+        currentOperationDisplay.innerText = clickedBtn.innerText;
+        return;
+    }
+    currentOperationDisplay.innerText += clickedBtn.innerText;
+}
+
+calculatorBtnSection.addEventListener("click", populateDisplay);
