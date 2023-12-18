bf952b9645eb9cb697cad0895e89cef0e135ed91
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2023-12-18 20:45:28 +0100

    Add delete/clear features

--- Calculator/script.js
+++ Calculator/script.js
@@ -3,12 +3,15 @@
 const calculatorBtnSection = document.querySelector(".calculator__btn-section");
 const currentOperationDisplay = document.querySelector(".calculator__current-operation");
 const resultDisplay = document.querySelector(".calculator__result");
+const clearBtn = document.querySelector(".clear-btn");
+const deleteBtn = document.querySelector(".delete-btn");
 
 function populateDisplay(e) {
     const clickedBtn = e.target;
 
     if(clickedBtn.classList.contains("operation-key")) return;
     if(!clickedBtn.matches("span")) return;
+    if(currentOperationDisplay.innerText.includes(".") && clickedBtn.innerText === ".") return;
 
     if(["x", "+", "/", "-"].includes(clickedBtn.innerText)) {
         resultDisplay.innerText = `${currentOperationDisplay.innerText} ${clickedBtn.innerText}`;
@@ -23,4 +26,19 @@ function populateDisplay(e) {
     currentOperationDisplay.innerText += clickedBtn.innerText;
 }
 
+function clearDisplay() {
+    resultDisplay.innerText = "";
+    currentOperationDisplay.innerText = "0";
+}
+
+function deleteLastDigit() {
+    if(currentOperationDisplay.innerText.length === 1) {
+        currentOperationDisplay.innerText = "0";
+        return;
+    }
+    currentOperationDisplay.innerText = currentOperationDisplay.innerText.replace(/\d$/, " ");
+}
+
 calculatorBtnSection.addEventListener("click", populateDisplay);
+clearBtn.addEventListener("click", clearDisplay);
+deleteBtn.addEventListener("click", deleteLastDigit);
