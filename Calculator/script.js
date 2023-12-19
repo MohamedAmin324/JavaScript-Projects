c8f7b7103b82acc5328a9d880ffd91010f89c2f3
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2023-12-19 13:42:35 +0100

    Make the = button functional

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,33 +5,35 @@ const currentOperationDisplay = document.querySelector(".calculator__current-ope
 const resultDisplay = document.querySelector(".calculator__result");
 const clearBtn = document.querySelector(".clear-btn");
 const deleteBtn = document.querySelector(".delete-btn");
+const equalBtn = document.querySelector(".equal-btn");
 const MATH_OPERATIONS = ["x", "+", "/", "-"];
 
 function populateDisplay(e) {
     const clickedBtn = e.target;
+    const userInput = clickedBtn.innerText;
 
     if(clickedBtn.classList.contains("operation-key")) return;
     if(!clickedBtn.matches("span")) return;
-    if(currentOperationDisplay.innerText.includes(".") && clickedBtn.innerText === ".") return;
+    if(currentOperationDisplay.innerText.includes(".") && userInput === ".") return;
 
-    if(["x", "+", "/", "-"].includes(clickedBtn.innerText)) {
+    if(["x", "+", "/", "-"].includes(userInput)) {
+        if(resultDisplay.innerText.includes("=")) resultDisplay.innerText = "";
         if(!detectMathOperations()) {
-        resultDisplay.innerText = `${currentOperationDisplay.innerText} ${clickedBtn.innerText}`;
+        resultDisplay.innerText = `${currentOperationDisplay.innerText} ${userInput}`;
         currentOperationDisplay.innerText = "0";
         return;
         }
-        const firstPartItems = resultDisplay.innerText.split(" ");
-        const result = performMathEquation(Number(firstPartItems[0]), firstPartItems[1], Number(currentOperationDisplay.innerText));
-        resultDisplay.innerText = `${result} ${clickedBtn.innerText}`;
+        const result = performMathEquation();
+        resultDisplay.innerText = `${result} ${userInput}`;
         currentOperationDisplay.innerText = "0";
         return;
     }
 
     if(currentOperationDisplay.innerText === "0") {
-        currentOperationDisplay.innerText = clickedBtn.innerText;
+        currentOperationDisplay.innerText = userInput;
         return;
     }
-    currentOperationDisplay.innerText += clickedBtn.innerText;
+    currentOperationDisplay.innerText += userInput;
 }
 
 function clearDisplay() {
@@ -51,26 +53,37 @@ function detectMathOperations() {
     return MATH_OPERATIONS.some((sign)=> resultDisplay.innerText.includes(sign));
 }
 
-function performMathEquation(operandOne, operator, operandTwo) {
+function performMathEquation() {
+    const firstPartItems = resultDisplay.innerText.split(" ");
+    const [operandOne, operator] = firstPartItems;
+    const operandTwo = Number(currentOperationDisplay.innerText);
     switch(operator) {
         case "+": {
-            return operandOne + operandTwo;
+            return Number(operandOne) + operandTwo;
         }
 
         case "-": {
-            return operandOne - operandTwo;
+            return Number(operandOne) - operandTwo;
         }
 
         case "x": {
-            return operandOne * operandTwo;
+            return Number(operandOne) * operandTwo;
         }
 
         case "/": {
-            return operandOne / operandTwo;
+            return Number(operandOne) / operandTwo;
         }
     }
+}
+
+function displayEquality() {
+    if(!detectMathOperations()) return;
+    const operationResult = performMathEquation();
+    resultDisplay.innerText = `${resultDisplay.innerText} ${currentOperationDisplay.innerText} =`;
+    currentOperationDisplay.innerText = operationResult;
 }
 
 calculatorBtnSection.addEventListener("click", populateDisplay);
 clearBtn.addEventListener("click", clearDisplay);
 deleteBtn.addEventListener("click", deleteLastDigit);
+equalBtn.addEventListener("click", displayEquality);
